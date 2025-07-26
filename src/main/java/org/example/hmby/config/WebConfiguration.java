63558d8aac94034bd3bbc3cbd7f49db2f1204352
ws <shu.wang@greencloud.com>
2025-07-26 18:27:59 +0800

    8

--- src/main/java/org/example/hmby/config/WebConfiguration.java
+++ src/main/java/org/example/hmby/config/WebConfiguration.java
@@ -25,6 +25,35 @@ public class WebConfiguration {
             public void addViewControllers(@NotNull ViewControllerRegistry registry) {
                 registry.addViewController("/").setViewName("redirect:/web/index");
             }
+
+            @Override
+            public void addResourceHandlers(@NotNull ResourceHandlerRegistry registry) {
+                registry.addResourceHandler("/web/**")
+                        .addResourceLocations("classpath:/dist/")
+                        .resourceChain(true)
+                        .addResolver(new PathResourceResolver() {
+                            @Override
+                            protected Resource getResource(@NotNull String resourcePath, @NotNull Resource location) throws IOException {
+                                Resource requestedResource = location.createRelative(resourcePath);
+                                // 如果资源不存在，则返回 index.html
+                                return requestedResource.exists() && requestedResource.isReadable()
+                                        ? requestedResource
+                                        : location.createRelative("index.html");
+                            }
+                        });
+            }
         };
+    }
+
+    @Bean
+    public CorsFilter corsFilter() {
+        final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowCredentials(true);
+        config.setAllowedOrigins(Collections.singletonList("http://localhost:5173"));
+        config.setAllowedHeaders(Arrays.asList("Origin", "Content-Type", "Accept", "responseType", "Authorization"));
+        config.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "OPTIONS", "DELETE", "PATCH"));
+        source.registerCorsConfiguration("*", config);
+        return new CorsFilter(source);
     }
 }
